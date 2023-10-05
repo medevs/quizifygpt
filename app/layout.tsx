6888b013b8ad11ebbf0bf8f4f27901765db026db
ahmed <oublihi.a@gmail.com>
2023-10-05 11:48:16 +0200

    navbar & signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
         <Providers>
+          <Navbar />
           {children}
         </Providers>
       </body>
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,22 @@
+"use client";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+//   useQuery,
+// } from "@tanstack/react-query";
+// const queryClient = new QueryClient();
 
-type Props = {
-  children: React.ReactNode;
-}
-
-const Providers = ({ children }: Props) => {
+const Providers = ({ children }: ThemeProviderProps) => {
   return (
-    <SessionProvider>{children}</SessionProvider>
+    // <QueryClientProvider client={queryClient}>
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        <SessionProvider>{children}</SessionProvider>
+      </NextThemesProvider>
+    // </QueryClientProvider>
   );
 };
 
